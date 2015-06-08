2901a7da530b8b8d67ed66a2f2d2dbf4f461982e
ErkanU <erkan.ulker1@gmail.com>
2015-06-08 16:50:22 +0200

    2.1-4

--- src/Getting_started.java
+++ src/Getting_started.java
@@ -10,23 +10,88 @@ class GettingStarted {
 
     public static void main(String[] args) {
 
-        int example[] = {5,4,3,2,1};
+        // Q2.1-1
+        System.out.println("==== Insertion sort ====");
+        int example[] = {31,41,59,26,41,58};
         printArray(insertionSort(example));
+
+        // Q2.1-2
+        System.out.println("==== Decreasing Insertion sort ====");
+        printArray(decreasingInsertionSort(example));
+
+        // Q2.1-4
+        System.out.println("==== Adding Binary integers ====");
+        int A[] = {1,0,0,1,0,1};
+        int B[] = {1,1,0,0,1,1};
+        printArray(addBinaryInteger(A, B));
+
     }
 
     public static int[] insertionSort(int[] A) {
         int i;
         int key;
-        for (int j = 2; j < A.length; j++) {
+        for (int j = 1; j < A.length; j++) {
             key = A[j];
             i = j - 1;
-            while (i < 0 && A[i] > key) {
+            while (i >= 0 && A[i] > key) {
                 A[i + 1] = A[i];
                 i--;
             }
             A[i + 1] = key;
         }
         return A;
+    }
+
+    public static int[] decreasingInsertionSort(int[] A) {
+        int i;
+        int key;
+        for (int j = A.length - 2; j >= 0; j--) {
+            key = A[j];
+            i = j + 1;
+            while (i < A.length && A[i] > key) {
+                A[i - 1] = A[i];
+                i++;
+            }
+            A[i - 1] = key;
+        }
+        return A;
+    }
+
+    // we suppose that the arrays only contain 1s and 0s
+    // the binary number is read from left to right (reverse)
+    public static int[] addBinaryInteger(int[] A, int[] B) {
+        if (A.length != B.length) {
+            System.out.println("A and B must have the same size");
+            return null;
+        }
+
+        int[] C = new int[A.length + 1];
+        int remainder = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != B[i]) {
+                if (remainder == 1) {
+                    C[i] = 0;
+                    remainder = 1;
+                } else {
+                    C[i] = 1;
+                    remainder = 0;
+                }
+            } else {
+                if (A[i] == 1) {
+                    if (remainder == 1) {
+                        C[i] = 1;
+                    } else {
+                        C[i] = 0;
+                    }
+                    remainder = 1;
+                } else {
+                    C[i] = remainder;
+                    remainder = 0;
+                }
+            }
+        }
+        C[C.length - 1] = remainder;
+        return C;
     }
 
     public static void printArray(int[] A) {
@@ -36,6 +101,6 @@ class GettingStarted {
             if (i != A.length - 1)
                 System.out.print(",");
         }
-        System.out.print("]");
+        System.out.println("]");
     }
 }
